6d22ed67839177d57749fe4017c76a9d1b619a9c
rakibul181 <rakibulhasan0641@gmail.com>
2022-10-05 11:35:24 +0600

    ready to daploy --- module 48-49 done

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -6,55 +6,55 @@ import './shop.css';
 
 const Shop = () => {
 
-    const[products, setProducts] = useState([])
-    const[cart, setCart] =useState([])
-   
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
 
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('products.json')
-        .then(res=> res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
 
-    },[])
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         const storedCart = GetDataFromLS()
-        const CartProduct =[]
+        const CartProduct = []
         for (const id in storedCart) {
-            const addedProduct =  products.find(product => product.id === id)
-            if(addedProduct){
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
                 const quantity = storedCart[id]
                 addedProduct.quantity = quantity
                 // console.log(addedProduct)
                 CartProduct.push(addedProduct)
             }
             setCart(CartProduct)
-        
-            
-        }
-    },[products])
 
-    const handelAddToCart = (sProduct)=>{
+
+        }
+    }, [products])
+
+    const handelAddToCart = (sProduct) => {
         // console.log(product)
         let newCart
-        
+
         const quantity = cart.find(product => product.id === sProduct.id)
-        if(!quantity){
+        if (!quantity) {
             sProduct.quantity = 1
             newCart = [...cart, sProduct]
         }
-        else{
-            const rest = cart.filter(product => product.id !== sProduct.id) 
-            const newQuantity = sProduct.quantity +1
-            sProduct[quantity] = newQuantity 
+        else {
+            const rest = cart.filter(product => product.id !== sProduct.id)
+            const newQuantity = sProduct.quantity + 1
+            sProduct[quantity] = newQuantity
             newCart = [...rest, sProduct]
         }
         newCart = [...cart, sProduct]
-        
+
         setCart(newCart)
         // console.log(cart)
 
-         
+
         addToLS(sProduct.id)
 
         // addToDb(product.id)
@@ -67,16 +67,16 @@ const Shop = () => {
             <div className="product-container">
                 {
                     products.map(product => <Product
-                        product ={product}
-                        key ={product.id}
-                        handelAddToCart ={handelAddToCart}
-                        ></Product>)
+                        product={product}
+                        key={product.id}
+                        handelAddToCart={handelAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3 className='summary'>Order  Summary</h3>
                 <Cart cart={cart}></Cart>
-                
+
             </div>
         </div>
     );
